a9256c86470d4541e8c1c31532268e1318df1004
nikohansen <nikolaus.hansen@inria.fr>
2018-05-27 13:06:34 +0200

    various smallish changes and clean up of code and docstrings
    method merge is now functional, expensiv assertions can be
    controlled via a class and an instance attribute.

--- python/moarchiving.py
+++ python/moarchiving.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-"""This module contains, for the time being, a single class: a
-bi-objective nondominated archive as sorted list with incremental
+"""This module contains, for the time being, a single MOO archive class.
+
+A bi-objective nondominated archive as sorted list with incremental
 update in logarithmic time.
 
 """
 from __future__ import division, print_function, unicode_literals
 __author__ = "Nikolaus Hansen and ..."
 __license__ = "BSD 3-clause"
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 del division, print_function, unicode_literals
 
+# from collections import deque  # does not support deletion of slices!?
 import bisect as _bisect # to find the insertion index efficiently
 import warnings
 
@@ -17,7 +19,8 @@ class BiobjectiveNondominatedSortedList(list):
     """A sorted list of non-dominated unique objective-pairs.
 
     Non-domination here means smaller in at least one objective. The list is
-    sorted (naturally) by the first objective first.
+    sorted (naturally) by the first objective. No equal entries in either
+    objective exist in the list (assuming it is in a consistent state).
 
     The operation
 
@@ -28,7 +31,8 @@ class BiobjectiveNondominatedSortedList(list):
     nondominated sorted archive.
 
     Afterwards, the methods `add` and `add_list` keep the list always
-    in a consistent state. Removing elements with `pop` is consistent too.
+    in a consistent state. Removing elements with `pop` or `del` is
+    consistent too.
 
     >>> a = BiobjectiveNondominatedSortedList([[1,0.9], [0,1], [0,2]])
     >>> a
@@ -40,24 +44,22 @@ class BiobjectiveNondominatedSortedList(list):
     [[-1.146, -0.11], [-0.749, -1.188]]
     >>> a._asserts()  # consistency assertions
 
-    TODO: write more example doctest (e.g. with given `reference_point`)
-
     Details: This list doesn't prevent the user to insert a new element
     anywhere and hence get into an inconsistent state. Inheriting from
     `sortedcontainers.SortedList` would ensure that the `list` remains
     at least sorted.
 
     See also:
-        https://pypi.org/project/sortedcontainers
-        https://code.activestate.com/recipes/577197-sortedcollection/
-        https://pythontips.com/2016/04/24/python-sorted-collections/
+    https://pypi.org/project/sortedcontainers
+    https://code.activestate.com/recipes/577197-sortedcollection/
+    https://pythontips.com/2016/04/24/python-sorted-collections/
 
     """
+    make_expensive_asserts = False  # used as default value
     def __init__(self,
                  list_of_f_pairs=None,
                  reference_point=None,
-                 sort=sorted,
-                 make_assertions=True):
+                 sort=sorted):
         """`list_of_f_pairs` does not need to be sorted.
 
         f-pairs beyond the `reference_point` are pruned away. The
@@ -69,7 +71,7 @@ class BiobjectiveNondominatedSortedList(list):
         CAVEAT: the interface, in particular the positional interface
         may change in future versions.
         """
-        self._make_expensive_asserts = make_assertions
+        self.make_expensive_asserts = BiobjectiveNondominatedSortedList.make_expensive_asserts
         if list_of_f_pairs is not None and len(list_of_f_pairs):
             try:
                 list_of_f_pairs = list_of_f_pairs.tolist()
@@ -79,26 +81,34 @@ class BiobjectiveNondominatedSortedList(list):
                 raise ValueError("need elements of len 2, got %s"
                                  " as first element" % str(list_of_f_pairs[0]))
             list.__init__(self, sort(list_of_f_pairs))
+            # super(BiobjectiveNondominatedSortedList, self).__init__(sort(list_of_f_pairs))
         if reference_point is not None:
             self.reference_point = list(reference_point)
         else:
             self.reference_point = reference_point
         self.prune()  # remove dominated entries, uses in_domain, hence ref-point
         self._set_hypervolume()
-        self._make_expensive_asserts and self._asserts()
+        self.make_expensive_asserts and self._asserts()
         # TODO: here we may want to set up a contributing _hypervolumes_list
         # see compute_hypervolumes
 
     def add(self, f_pair):
-        """insert `f_pair` in `self` if and only if it is nondominated.
+        """insert `f_pair` in `self` if it is not (weakly) dominated.
 
         Return index at which the insertion took place or `None`. The
-        list remains to be sorted in the process.
+        list remains sorted in the process.
 
-        Details: For performance reasons, `insert` is avoided in favor
-        of `__setitem__`, if possible.
+        The list remains non-dominated with unique elements, which
+        means that some or many or even all of its present elements may
+        be removed.
+
+        Implementation detail: For performance reasons, `insert` is
+        avoided in favor of `__setitem__`, if possible.
         """
         f_pair = list(f_pair)  # convert array to list
+        if len(f_pair) != 2:
+            raise ValueError("argument `f_pair` must be of length 2, was"
+                             " ``%s``" % str(f_pair))
         if not self.in_domain(f_pair):
             return None
         idx = self.bisect_left(f_pair)
@@ -106,15 +116,22 @@ class BiobjectiveNondominatedSortedList(list):
             return None
         assert idx == len(self) or not f_pair == self[idx]
         # here f_pair now is non-dominated
-        return self._add_at(idx, f_pair)
+        self._add_at(idx, f_pair)
+        # self.make_expensive_asserts and self._asserts()
+        return idx
 
     def _add_at(self, idx, f_pair):
-        """add `f_pair` at position `idx` iff it is nondominated by its neighbours
+        """add `f_pair` at position `idx` and remove dominated elements.
+
+        This method assumes that `f_pair` is not weakly dominated by
+        `self` and that `idx` is the correct insertion place e.g.
+        acquired by `bisect_left`.
         """
         if idx == len(self) or f_pair[1] > self[idx][1]:
             self.insert(idx, f_pair)
             self._add_HV(idx)
-            return idx
+            # self.make_expensive_asserts and self._asserts()
+            return
         # here f_pair now dominates self[idx]
         idx2 = idx + 1
         while idx2 < len(self) and f_pair[1] <= self[idx2][1]:
@@ -126,7 +143,8 @@ class BiobjectiveNondominatedSortedList(list):
         self[idx] = f_pair  # on long lists [.] is much cheaper than insert
         del self[idx + 1:idx2]  # can make `add` 20x faster
         self._add_HV(idx)
-        return idx
+        assert len(self) >= 1
+        # self.make_expensive_asserts and self._asserts()
 
     def add_list(self, list_of_f_pairs):
         """insert a list of f-pairs which doesn't need to be sorted.
@@ -153,14 +171,18 @@ class BiobjectiveNondominatedSortedList(list):
         nb = len(self)
         for f_pair in list_of_f_pairs:
             self.add(f_pair)
+        self.make_expensive_asserts and self._asserts()
         return len(self) - nb
 
     def merge(self, list_of_f_pairs):
-        """merge a sorted list of f-pairs which doesn't need to be nondominated.
+        """merge in a sorted list of f-pairs which doesn't need to be nondominated.
 
         Return number of actually inserted f-pairs.
+
+        Details: merging 200 into 100_000 takes 3e-4s vs 4e-4s with
+        `add_list`.
         """
-        raise NotImplementedError("TODO: proof read and test, use `add_list` for the time being")
+        # warnings.warn("merge was never thoroughly tested, use `add_list`")
         nb = len(self)
         idx = 0
         for f_pair in list_of_f_pairs:
@@ -168,15 +190,19 @@ class BiobjectiveNondominatedSortedList(list):
                 continue
             f_pair = list(f_pair)  # convert array to list
             idx = self.bisect_left(f_pair, idx)
+            if self.dominates_with(idx - 1, f_pair) or self.dominates_with(idx, f_pair):
+                continue
             self._add_at(idx, f_pair)
-        self._set_hypervolume()
+        self.make_expensive_asserts and self._asserts()
         return len(self) - nb
 
     def bisect_left(self, f_pair, lowest_index=0):
         """return index where `f_pair` may need to be inserted.
 
-        Smaller indices have a strictly better f1 value or equal f1 and better
-        f2 value.
+        Smaller indices have a strictly better f1 value or they have
+        equal f1 and better f2 value.
+
+        `lowest_index` restricts the search from below.
 
         Details: This method does a binary search in `self` using
         `bisect.bisect_left`.
@@ -194,6 +220,7 @@ class BiobjectiveNondominatedSortedList(list):
         True
         >>> a.dominates([-1, 33]) or a.dominates([33, -1])
         False
+        >>> a._asserts()
 
         See also `bisect_left` to find the closest index.
         """
@@ -203,7 +230,7 @@ class BiobjectiveNondominatedSortedList(list):
         return False
 
     def dominates_with(self, idx, f_pair):
-        """return `True` if ``self[idx]`` dominates (or is equal to) `f_pair`.
+        """return `True` if ``self[idx]`` dominates or is equal to `f_pair`.
 
         Otherwise return `False` or `None` if `idx` is out-of-range.
 
@@ -257,7 +284,22 @@ class BiobjectiveNondominatedSortedList(list):
         """return `True` if `f_pair` is dominating the reference point,
 
         `False` otherwise. `True` means that `f_pair` contributes to
-        the hypervolume.
+        the hypervolume if not dominated by other elements.
+
+        `f_pair` may also be an index in `self` in which case
+        ``self[f_pair]`` is tested to be in-domain.
+
+        >>> from moarchiving import BiobjectiveNondominatedSortedList as NDA
+        >>> a = NDA([[2.2, 0.1], [0.5, 1]], reference_point=[2, 2])
+        >>> assert len(a) == 1
+        >>> a.in_domain([0, 0])
+        True
+        >>> a.in_domain([2, 1])
+        False
+        >>> all(a.in_domain(ai) for ai in a)
+        True
+        >>> a.in_domain(0)
+        True
 
         TODO: improve name?
         """
@@ -265,6 +307,12 @@ class BiobjectiveNondominatedSortedList(list):
             reference_point = self.reference_point
         if reference_point is None:
             return True
+        try:
+            f_pair = self[f_pair]
+        except TypeError:
+            pass
+        except IndexError:
+            raise  # return None
         if (f_pair[0] >= reference_point[0] or
             f_pair[1] >= reference_point[1]):
             return False
@@ -298,7 +346,7 @@ class BiobjectiveNondominatedSortedList(list):
         if self.reference_point is None:
             raise ValueError("to compute the hypervolume a reference"
                              " point is needed (must be given initially)")
-        if self._make_expensive_asserts:
+        if self.make_expensive_asserts:
             assert abs(self._hypervolume - self.compute_hypervolume(self.reference_point)) < 1e-12
         return self._hypervolume
 
@@ -318,7 +366,7 @@ class BiobjectiveNondominatedSortedList(list):
         return self._hypervolume
 
     def compute_hypervolume(self, reference_point):
-        """return hypervolume w.r.t. reference_point"""
+        """return hypervolume w.r.t. `reference_point`"""
         if reference_point is None:
             raise ValueError("to compute the hypervolume a reference"
                              " point is needed (was `None`)")
@@ -376,7 +424,7 @@ class BiobjectiveNondominatedSortedList(list):
         return dHV
 
     def _add_HV(self, idx):
-        """add contributing hypervolume of self[idx]"""
+        """add contributing hypervolume of ``self[idx]``"""
         if self.reference_point is None:
             return None
         assert 0 <= idx < len(self)  # idx is not a public interface, hence assert is fine
@@ -399,9 +447,18 @@ class BiobjectiveNondominatedSortedList(list):
         return dHV
 
     def prune(self):
-        """remove dominated entries assuming that the list is sorted.
+        """remove dominated or equal entries assuming that the list is sorted.
 
         Return number of dropped elements.
+
+        Implementation details: pruning from right to left may be
+        preferable, because list.insert(0) is O(n) while list.append is
+        O(1), however it is not possible with the given sorting: in
+        principle, the first element may dominate all others, which can
+        only be discovered in the last step when traversing from right
+        to left. This suggests that reverse sort may be better for
+        pruning or we should inherit from `collections.deque` instead
+        from `list`, but `deque` seems not to support deletion of slices.
         """
         nb = len(self)
         i = 1
@@ -420,13 +477,25 @@ class BiobjectiveNondominatedSortedList(list):
         assert sorted(self) == self
         for pair in self:
             assert self.count(pair) == 1
-        assert BiobjectiveNondominatedSortedList(self, make_assertions=False) == self
+        tmp = BiobjectiveNondominatedSortedList.make_expensive_asserts
+        BiobjectiveNondominatedSortedList.make_expensive_asserts = False
+        assert BiobjectiveNondominatedSortedList(self) == self
+        BiobjectiveNondominatedSortedList.make_expensive_asserts = tmp
         for pair in self:
             assert self.dominates(pair)
             assert not self.dominates([v - 0.001 for v in pair])
         if self.reference_point is not None:
             assert abs(self._hypervolume - self.compute_hypervolume(self.reference_point)) < 1e-11
-
+        # asserts using numpy for convenience
+        try:
+            import numpy as np
+        except ImportError:
+            warnings.warn("asserts using numpy omitted")
+        else:
+            if len(self) > 1:
+                diffs = np.diff(self, 1, 0)
+                assert all(diffs[:, 0] > 0)
+                assert all(diffs[:, 1] < 0)
 
 if __name__ == "__main__":
     import doctest
