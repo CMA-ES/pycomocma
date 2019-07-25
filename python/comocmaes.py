de44ecfa7f3b4acc8abbf96cb70f69819ad4f497
TOURE Cheikh Saliou <cheikh-saliou.toure@inria.fr>
2019-07-25 14:15:52 +0200

    sofomore framework

---
+++ python/comocmaes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import numpy as np
+import cma
+from cma import interfaces
+from nondominatedarchive import NonDominatedList as NDL
+from moarchiving import BiobjectiveNondominatedSortedList as BNDS
+
+class Comocmaes(object):
+    """
+    """
+    
