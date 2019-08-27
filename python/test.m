34f6969634ad54602cd7d34acee3cf2f04d50ed5
TOURE Cheikh Saliou <cheikh-saliou.toure@inria.fr>
2019-08-27 19:53:36 +0200

    routine commit

--- python/test.m
+++ python/test.m
@@ -1,12 +1,12 @@
 opts = COMOCMAES();
-opts.nPop = 4;
+opts.nPop = 5;
 nObj = 2;
 nVar = 10;
 xstart = ones(1, nVar);
 sigma0 = 0.2;
-opts.bounds = [0.5, 0.9];
+opts.bounds = [0.2, 0.9];
 opts.maxiter = 400;
-%opts.verb_display = 100;
-% options for resume
-opts.OkResume = 1;
+opts.OkResume = 1; % option for resume: `1` means we save the optimization
+% after each iteration (for difficult problems), and for `0` we skip the saving
+
 [paretoFront, paretoSet, out] = COMOCMAES('bi_sphere', nObj, xstart, sigma0, opts);
