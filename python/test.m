73104bc738734d290a4e83a0f182f4c9ae23e85d
TOURE Cheikh Saliou <cheikh-saliou.toure@inria.fr>
2019-08-29 12:46:38 +0200

    routine commit

--- python/test.m
+++ python/test.m
@@ -6,7 +6,8 @@ xstart = ones(1, nVar);
 sigma0 = 0.2;
 opts.bounds = [0.2, 0.9];
 opts.maxiter = 400;
-opts.OkResume = 1; % option for resume: `1` means we save the optimization
+opts.logger = 1; % to write date
+opts.OkResume = 0; % option for resume: `1` means we save the optimization
 % after each iteration (for difficult problems), and for `0` we skip the saving
 
 [paretoFront, paretoSet, out] = COMOCMAES('bi_sphere', nObj, xstart, sigma0, opts);
