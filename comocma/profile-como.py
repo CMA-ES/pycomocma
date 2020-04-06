99505e49a4b0de5665e4bff136b22541ca59f6f3
brockhof <dimo.brockhoff@inria.fr>
2020-04-06 16:44:07 +0200

    update profile-como.py

--- comocma/profile-como.py
+++ comocma/profile-como.py
@@ -11,18 +11,21 @@ list_of_solvers = como.get_cmas(-5 + 10 * np.random.rand(num_kernels, dimension)
                                 sigma0,  # inopts=cmaopts
                                 {'tolx': 10 ** -4, 
                                  'tolfunrel': 1e-2,
-                                 'maxiter': 1e9}
+                                 'maxiter': 1e9,
+                                 'verbose': -9,
+                                 'signals_filename': None,
+                                 }
                                 )  # produce `num_kernels cma instances`
 moes = como.Sofomore(list_of_solvers, opts={'archive': False,
                                      #'restart': como.random_restart_kernel},
-                                     'restart': como.best_chv_restart_kernel,
-                                     #'restart': como.best_chv_or_random_restart_kernel,
+                                     #'restart': como.best_chv_restart_kernel,
+                                     'restart': como.RampUpSelector([como.random_restart_kernel, como.best_chv_restart_kernel]),
                                      'continue_stopped_kernel': False,
                                      'random_restart_on_domination': False},
                                      reference_point = [11,11])
 fitness = como.FitFun(cma.ff.sphere, lambda x: cma.ff.sphere(x-1))
 iter = 1
-while not moes.stop() and iter < 1000:
+while not moes.stop() and iter < 5000:
     solutions = moes.ask()
     objective_values = [fitness(x) for x in solutions]
     moes.tell(solutions, objective_values)
@@ -31,7 +34,7 @@ while not moes.stop() and iter < 1000:
 profilefile)
 s = pstats.Stats(profilefile)
 from pstats import SortKey
-s.sort_stats('tottime').print_stats(100)
+s.sort_stats('tottime').print_stats(10)
 
 s.sort_stats('cumulative').print_stats(10)
 # s.sort_stats('time')
