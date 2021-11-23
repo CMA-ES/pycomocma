482918f0be90f89c47fe1438b9be0e795a19f8e5
Eugénie Marescaux <eugenie.marescaux@inria.fr>
2021-11-23 16:08:32 +0100

    Add the possibility to use personalized functions at the end of the usual storing. Modify the loading so that the data which is read from the file is stored in self.data and reloaded from the file only when needed. Separate the plotting function into individual plotting functions. Create a function, 'plot_everything' which call all plotting functions of the class.

--- comocma/como_logger.py
+++ comocma/como_logger.py
@@ -5,7 +5,11 @@ import datetime
 from time import time
 
 class COMOPlot_Callback:
-    def __init__(self):
+    def __init__(self, storing_funs=[]):
+        '''
+        It takes a facultative parameter:
+        - 'storing_funs': a list of functions which will be called at the end of any self.store call
+        '''
         # create the directory where the data will be stored
         path = os.getcwd()
         name_save = datetime.datetime.now().strftime("%y%m%d_%Hh%Mm%Ss")
@@ -25,6 +29,16 @@ class COMOPlot_Callback:
         self.dir = path2
         # initialization of the offset for convergence speed plotting, which is ideally the hypervolume of the Pareto front
         self.offset = None
+        # remember a list of storing functions which will be called with arguments self and moes on top of the standard storing function
+        # each time the function self.store is called
+        self.storing_funs = storing_funs
+        # dictionary which will later contain the data
+        self.data = None
+        # number of times the function store have been called
+        self.num_calls = 0
+        # last call stored in self.data
+        # when self.num_data < self.num_calls, it means that the data in self.data is not up to date
+        self.num_data = 0
 
     def store0(self, name, v):
         """Store the value v in the file name.txt ."""
@@ -65,9 +79,20 @@ class COMOPlot_Callback:
         # store the hypervolume of the archive
         self.store0("hv_archive", moes.archive.hypervolume)
         self.store0("hv_incumbents", float(ND.hypervolume))
+        # update the number of times this function have been called
+        self.num_calls += 1
+
+        # call the storing functions
+        for fun in self.storing_funs:
+            fun(self, moes)
     
     def load(self):
-        """Read all the data stored in self.dir and return them as a dictionary."""
+        '''
+        Load the data stored in the directory self.dir if it has not been done yet and store it in self.data as a dictionary.
+        Return this dictionary. 
+        '''
+        if self.num_data == self.num_calls: # the stored data has already been read entirely and stored in self.data
+            return self.data
         dic = dict()
         for file in os.listdir(self.dir):
             with open(self.dir + file) as f:
@@ -77,22 +102,30 @@ class COMOPlot_Callback:
                     dic[key] = [eval(l) for l in lines]
                 except:
                     dic[key] = lines
+        self.data = dic
+        self.num_data = self.num_calls
         return dic
     
-    def plot(self):
-        """Plot whathever we find interesting to plot. Be careful, the first data corresponds to the be"""
+    def plot_everything(self):
+        """Call every method of the class whose name begins by 'plot_' and which is not 'plot_everything' itself."""
+        fun_names = [name for name in dir(self) if name[:5]=='plot_' and name!= 'plot_everything']
+        for name in fun_names:
+            getattr(self, name)()
+
+    def plot_proportion_dominated_final_incumbents(self, dic=None):
+        '''Plot the proportion of dominated final incumbents.'''
         dic = self.load()
         n_runs = dic["num_completedruns"][-1]
-        n_iter = len(dic["hv_incumbents"])
-
-        # Plot the proportion of dominated final incumbents
         plt.figure()
         plt.plot(dic["num_completedruns"], [dic["num_dominatedfinalincumbents"][i] / (i+1) for i in range(n_runs)], '.')
         plt.xlabel("number of runs completed")
         plt.ylabel("proportion of dominated final incumbents")
         plt.title("proportion of final incumbents which are now dominated")
 
-        # Plot the number of iterations per restart
+    def plot_iterations_per_restart(self):
+        '''Plot the number of iterations per restart.'''
+        dic = self.load()
+        n_runs = dic["num_completedruns"][-1]
         plt.figure()
         plt.plot([dic["num_completedruns"][i+1] for i in range(n_runs-1) if dic["kindrestart"][i]=="best_chv\n"],[dic["iter_newrun"][i+1] - dic["iter_newrun"][i]  for i in range(n_runs - 1) if dic["kindrestart"][i]=="best_chv\n"], '.')
         plt.plot([dic["num_completedruns"][i+1] for i in range(n_runs-1) if dic["kindrestart"][i]=="random\n"],[dic["iter_newrun"][i+1] - dic["iter_newrun"][i]  for i in range(n_runs - 1) if dic["kindrestart"][i]=="random\n"], '.')
@@ -101,14 +134,17 @@ class COMOPlot_Callback:
         plt.ylabel("number of iterations of the last run")
         plt.title("Number of iterations per run")
 
-        # Plot the convergence speed
+    def plot_convergence_speed(self):
+        '''Plot the convergence speed.'''
+        dic = self.load()
         if self.offset is None:
             self.offset = dic["hv_archive"][-1]
+        n_iters = len(dic["hv_incumbents"])
         plt.figure()
-        plt.semilogy(range(1, n_iter + 1),[self.offset - dic["hv_incumbents"][i] for i in range(n_iter)], 'g')
-        plt.semilogy(range(1, n_iter + 1),[self.offset - dic["hv_archive"][i] for i in range(n_iter)], 'b')
-        plt.semilogy(dic["iter_newrun"], [self.offset - dic["hv_incumbents"][i] for i in range(n_iter) if i in dic["iter_newrun"]], '.g')
-        plt.semilogy(dic["iter_newrun"], [self.offset - dic["hv_archive"][i] for i in range(n_iter) if i in dic["iter_newrun"]], '.b')
+        plt.semilogy(range(1, n_iters + 1),[self.offset - dic["hv_incumbents"][i] for i in range(n_iters)], 'g')
+        plt.semilogy(range(1, n_iters + 1),[self.offset - dic["hv_archive"][i] for i in range(n_iters)], 'b')
+        plt.semilogy(dic["iter_newrun"], [self.offset - dic["hv_incumbents"][i] for i in range(n_iters) if i in dic["iter_newrun"]], '.g')
+        plt.semilogy(dic["iter_newrun"], [self.offset - dic["hv_archive"][i] for i in range(n_iters) if i in dic["iter_newrun"]], '.b')
         plt.legend(["$S=$final incumbents only", "$S=$archive"])
         plt.xlabel("iterations")
         plt.ylabel("offset - $HV_r(S)$ ")
